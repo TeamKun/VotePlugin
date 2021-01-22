052a1be14478f136903ab5bbb0f903c105579588
krmn <38986306+krmn08@users.noreply.github.com>
2021-01-22 22:21:42 +0900

    commit

--- src/main/java/net/teamfruit/voteplugin/VoteAPI.java
+++ src/main/java/net/teamfruit/voteplugin/VoteAPI.java
@@ -16,8 +16,9 @@ public class VoteAPI {
     private boolean isVoting;
     private Consumer<VoteResult> listener;
     private Set<UUID> votedPlayers;
-    private Map<UUID, Integer> countMap;
+    private Map<String, Integer> countMap;
     private Map<String, String> destinations;
+    private Map<String, UUID> uniqueIdMap;
     private VoteResult result;
 
     private VoteAPI() {
@@ -27,6 +28,7 @@ public class VoteAPI {
         isVoting = false;
         listener = null;
         votedPlayers = new HashSet<>();
+        uniqueIdMap = new HashMap<>();
         countMap = new HashMap<>();
         destinations = new HashMap<>();
         result = null;
@@ -54,21 +56,27 @@ public class VoteAPI {
         }
         votedPlayers.add(player.getUniqueId());
 
-        UUID uuid = dest.getUniqueId();
-        boolean isVoted = countMap.containsKey(uuid);
-        countMap.put(uuid, isVoted ? countMap.get(uuid) + 1 : 1);
+        String name = dest.getName();
+        boolean isVoted = countMap.containsKey(name);
+        countMap.put(name, isVoted ? countMap.get(name) + 1 : 1);
         destinations.put(player.getName(), dest.getName());
+        uniqueIdMap.put(dest.getName(), dest.getUniqueId());
     }
 
     public void beginVoting() {
+        if (isVoting) {
+            throw new IllegalStateException();
+        }
+
         Bukkit.broadcastMessage(ChatColor.GOLD + "投票を開始しました。");
         Bukkit.broadcastMessage(ChatColor.GREEN + "/v <プレイヤー名>で投票をしてください。");
 
         Bukkit.getOnlinePlayers().forEach(ScoreboardUtil::unrankedSidebarDisplay);
 
         votedPlayers.clear();
-        destinations.clear();
         countMap.clear();
+        destinations.clear();
+        uniqueIdMap.clear();
         result = null;
 
         isVoting = true;
@@ -82,14 +90,14 @@ public class VoteAPI {
     public void endVoting() {
         isVoting = false;
         Bukkit.broadcastMessage(ChatColor.GOLD + "投票を終了しました。");
-        List<Map.Entry<UUID, Integer>> list_entries = countMap.entrySet().stream()
+        List<Map.Entry<String, Integer>> list_entries = countMap.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toList());
 
         List<String> leaderboard = IntStream.range(0, 5)
                 .filter(i -> i < list_entries.size())
                 .mapToObj(i -> {
-                    Map.Entry<UUID, Integer> entry = list_entries.get(i);
+                    Map.Entry<String, Integer> entry = list_entries.get(i);
                     return "" + ChatColor.GREEN + ChatColor.AQUA + "" + (i + 1) + "位 " + ChatColor.GOLD + entry.getKey() + ChatColor.GRAY + " (" + ChatColor.GRAY + entry.getValue() + "票)";
                 })
                 .collect(Collectors.toList());
@@ -102,7 +110,7 @@ public class VoteAPI {
         ).toArray(String[]::new);
 
         Bukkit.getOnlinePlayers().forEach(p -> ScoreboardUtil.unrankedSidebarDisplay(p, s));
-        result = new VoteResult(countMap, destinations);
+        result = new VoteResult(countMap, destinations, uniqueIdMap);
         if (listener != null) {
             listener.accept(result);
             listener = null;
--- src/main/java/net/teamfruit/voteplugin/VoteResult.java
+++ src/main/java/net/teamfruit/voteplugin/VoteResult.java
@@ -4,11 +4,13 @@ import java.util.Map;
 import java.util.UUID;
 
 public class VoteResult {
-    private final UUID[] top;
-    private final Map<UUID, Integer> result;
+    private final String[] top;
+    private final Map<String, Integer> result;
     private final Map<String, String> destinations;
+    private final Map<String, UUID> uniqueIdMap;
 
-    public VoteResult(Map<UUID, Integer> result, Map<String, String> destinations) {
+    public VoteResult(Map<String, Integer> result, Map<String, String> destinations, Map<String, UUID> uniqueIdMap) {
+        this.uniqueIdMap = uniqueIdMap;
         int max = result.values()
                 .stream()
                 .mapToInt(Integer::intValue)
@@ -18,20 +20,24 @@ public class VoteResult {
                 .stream()
                 .filter(entry -> entry.getValue() == max)
                 .map(Map.Entry::getKey)
-                .toArray(UUID[]::new);
+                .toArray(String[]::new);
         this.result = result;
         this.destinations = destinations;
     }
 
-    public UUID[] getTop() {
+    public String[] getTop() {
         return top;
     }
 
-    public Map<UUID, Integer> getResult() {
+    public Map<String, Integer> getResult() {
         return result;
     }
 
     public Map<String, String> getDestinations() {
         return destinations;
+    }
+
+    public Map<String, UUID> getUniqueIdMap() {
+        return uniqueIdMap;
     }
 }
